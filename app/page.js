c0a085b659379956ab30febf4613658ca4908135
Cindyyeo12345 <e1336362@u.nus.edu>
2026-05-22 15:28:50 +0800

    feat: scaffold minimal nextjs app

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Next.js app is running</h1>
+      <p>This workspace now has a working dev server scaffold.</p>
+    </main>
+  );
+}
